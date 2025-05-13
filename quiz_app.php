d866be1235b673028ae3a56557e97bbbad76c89c
Mohammad Qumruzzaman <90256141+qumruzzaman@users.noreply.github.com>
2025-05-13 23:20:17 +0600

    Update quiz_app.php

--- quiz_app.php
+++ quiz_app.php
@@ -2,7 +2,7 @@
 // Terminal-Based Quiz Application
 $score = 0;
 echo "==== Welcome to Quiz ====\n";
-echo "1. Which symbol is used to declare a variable in PHP?\na) ? b) # c) $\n\nChoice your option(a/b/c): ";
+echo "1. Which symbol is used to declare a variable in PHP?\na) ? b) # c) $\nChoose your option(a/b/c): ";
 // Q1
 $answer1 = trim(fgets(STDIN));
 if($answer1 == 'c'){
@@ -12,7 +12,7 @@ if($answer1 == 'c'){
     echo "Wrong answer. Correct answer is (c)\n";
 }
 // Q2
-echo "2. What is the capital of Bangladesh?\na) Dhaka b) Delhi c) Paris\n\nChoice your option(a/b/c): ";
+echo "2. What is the capital of Bangladesh?\na) Dhaka b) Delhi c) Paris\nChoose your option(a/b/c): ";
 
 $answer2 = trim(fgets(STDIN));
 if($answer2 == 'a'){
@@ -22,10 +22,10 @@ if($answer2 == 'a'){
     echo "Wrong answer. Correct answer is (a)\n";
 }
 // Q3
-echo "2. What is correct answer of 2 + 2\na) 2 b) 4 c) 6\n\nChoice your option(a/b/c): ";
+echo "3. What is the correct answer for 2 + 2?\na) 2 b) 4 c) 6\nChoose your option(a/b/c): ";
 
-$answer2 = trim(fgets(STDIN));
-if($answer2 == 'b'){
+$answer3 = trim(fgets(STDIN));
+if($answer3 == 'b'){
     echo "Correct answer.\n";
     $score++;
 }else{
@@ -34,3 +34,5 @@ if($answer2 == 'b'){
 
 echo "==== Quiz finished ====\n";
 echo "You scored $score out of 3.\n";
+
+?>
